c2cf88461ac06854a3fa38b7392fc7c237f7adae
AmandaSilvaTeles <166659332+AmandaSilvaTeles@users.noreply.github.com>
2024-04-17 17:11:23 -0300

    Add files via upload

---
+++ src/JS/manuais.js
@@ -0,0 +1,191 @@
+const abrirModal1 = document.querySelector("#abrirModal1");
+const modal1 = document.querySelector("#dialog1");
+const fecharModal1 = document.querySelector("#fecharModal1")
+
+
+
+abrirModal1.onclick = function () {
+    modal1.show()    
+    modal2.close ()
+    modal3.close ()
+    modal4.close ()
+    modal5.close ()
+    modal6.close ()
+    modal7.close ()
+    modal8.close ()
+    modal9.close ()
+}
+
+fecharModal1.onclick = function () {
+    modal1.close ()
+}
+
+const abrirModal2 = document.querySelector("#abrirModal2");
+const modal2 = document.querySelector("#dialog2");
+const fecharModal2 = document.querySelector("#fecharModal2")
+
+abrirModal2.onclick = function () {
+    modal2.show()   
+    modal1.close () 
+    modal3.close ()
+    modal4.close ()
+    modal5.close ()
+    modal6.close ()
+    modal7.close ()
+    modal8.close ()
+    modal9.close ()
+
+}
+
+fecharModal2.onclick = function () {
+    modal2.close ()
+}
+
+const abrirModal3 = document.querySelector("#abrirModal3");
+const modal3 = document.querySelector("#dialog3");
+const fecharModal3 = document.querySelector("#fecharModal3")
+
+abrirModal3.onclick = function () {
+    modal3.show()   
+    modal1.close () 
+    modal2.close ()
+    modal4.close ()
+    modal5.close ()
+    modal6.close ()
+    modal7.close ()
+    modal8.close ()
+    modal9.close ()
+
+}
+
+fecharModal3.onclick = function () {
+    modal3.close ()
+}
+
+const abrirModal4 = document.querySelector("#abrirModal4");
+const modal4 = document.querySelector("#dialog4");
+const fecharModal4 = document.querySelector("#fecharModal4")
+
+abrirModal4.onclick = function () {
+    modal4.show()   
+    modal1.close () 
+    modal2.close ()
+    modal3.close ()
+    modal5.close ()
+    modal6.close ()
+    modal7.close ()
+    modal8.close ()
+    modal9.close ()
+
+}
+
+fecharModal4.onclick = function () {
+    modal4.close ()
+}
+
+const abrirModal5 = document.querySelector("#abrirModal5");
+const modal5 = document.querySelector("#dialog5");
+const fecharModal5 = document.querySelector("#fecharModal5")
+
+abrirModal5.onclick = function () {
+    modal5.show()   
+    modal1.close () 
+    modal2.close ()
+    modal3.close ()
+    modal4.close ()
+    modal6.close ()
+    modal7.close ()
+    modal8.close ()
+    modal9.close ()
+
+}
+
+fecharModal5.onclick = function () {
+    modal5.close ()
+}
+
+const abrirModal6 = document.querySelector("#abrirModal6");
+const modal6 = document.querySelector("#dialog6");
+const fecharModal6 = document.querySelector("#fecharModal6")
+
+abrirModal6.onclick = function () {
+    modal6.show()   
+    modal1.close () 
+    modal2.close ()
+    modal3.close ()
+    modal4.close ()
+    modal5.close ()
+    modal7.close ()
+    modal8.close ()
+    modal9.close ()
+
+    
+}
+
+fecharModal6.onclick = function () {
+    modal6.close ()
+}
+
+const abrirModal7 = document.querySelector("#abrirModal7");
+const modal7 = document.querySelector("#dialog7");
+const fecharModal7 = document.querySelector("#fecharModal7")
+
+abrirModal7.onclick = function () {
+    modal7.show()   
+    modal1.close () 
+    modal2.close ()
+    modal3.close ()
+    modal4.close ()
+    modal5.close ()
+    modal6.close ()
+    modal8.close ()
+    modal9.close ()
+
+    
+}
+
+fecharModal7.onclick = function () {
+    modal7.close ()
+}
+
+const abrirModal8 = document.querySelector("#abrirModal8");
+const modal8 = document.querySelector("#dialog8");
+const fecharModal8 = document.querySelector("#fecharModal8")
+
+abrirModal8.onclick = function () {
+    modal8.show()   
+    modal1.close () 
+    modal2.close ()
+    modal3.close ()
+    modal4.close ()
+    modal5.close ()
+    modal6.close ()
+    modal7.close ()    
+    modal9.close ()
+
+}
+
+fecharModal8.onclick = function () {
+    modal8.close ()
+}
+
+const abrirModal9 = document.querySelector("#abrirModal9");
+const modal9 = document.querySelector("#dialog9");
+const fecharModal9 = document.querySelector("#fecharModal9")
+
+abrirModal9.onclick = function () {
+    modal9.show()   
+    modal1.close () 
+    modal2.close ()
+    modal3.close ()
+    modal4.close ()
+    modal5.close ()
+    modal6.close ()
+    modal7.close ()
+    modal8.close ()
+    
+}
+
+fecharModal9.onclick = function () {
+    modal9.close ()
+}
